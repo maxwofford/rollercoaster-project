9cd56dc16fe2f2b49cfcc67ed7ab5962d86baaf6
MaxWofford <max.speed.wofford@gmail.com>
2014-05-20 01:03:38 -0700

    Removed userEmail from js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
 var ref = new Firebase("https://rollercoaster-2014.firebaseio.com/"); /*Initialize firebase*/
 
-var submit = function (userEmail) {
-    alert("sending email"); /*Popup window*/
-    var userEmail = document.getElementById("emailInput").value; /*Read the email the user typed in*/
- 	ref.push({email: userEmail}); /*Pushes data to firebase*/
- 	document.getElementById("submitButton").value = "Thank you!";
-    return false;
-}
-
-var submitFeedback = function (userEmail, userFeedback) {
+var submitFeedback = function (userFeedback) {
     alert("sending feedback"); /*Popup window*/
     var userFeedback = document.getElementById("feedbackInput").value;
-    var userEmail = document.getElementById("emailInput").value;
- 	ref.push({email: userEmail, feedback: userFeedback}); /*Pushes data to firebase*/
+    
+ 	ref.push({feedback: userFeedback}); /*Pushes data to firebase*/
  	document.getElementById("submitButton").value = "Thank you!";
     return false;
 }
